dd01a90cdd54e0533509d8ce35719fe34023aec2
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:11 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'PetSwipe-Match-App';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
